19e2e9810e6f24d018d8adf4ae7f8abd53a4ff22
JKioko <jkioko999@gmail.com>
2023-10-13 06:53:07 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -2,12 +2,11 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
-void print_alphabet(void)
-
+int main(void)
 {
-	print_alphabet();
-	return (0);
+    print_alphabet();
+    return (0);
 }
-
